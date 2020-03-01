dd8e1e065d5d0b388684096ac11c910346c8aa06
Arman Kolahan <arman.kolahan@tripix.tech>
2020-03-01 10:16:15 +0100

    viewPager added

--- app/src/main/java/com/armanco/formula/view/main/MainActivity.java
+++ app/src/main/java/com/armanco/formula/view/main/MainActivity.java
@@ -9,16 +9,16 @@ import androidx.viewpager.widget.ViewPager;
 import com.armanco.formula.R;
 import com.armanco.formula.data.models.Section;
 import com.armanco.formula.view.base.BaseActivity;
-import com.github.florent37.materialviewpager.MaterialViewPager;
-import com.github.florent37.materialviewpager.MaterialViewPagerHelper;
-import com.github.florent37.materialviewpager.header.HeaderDesign;
+import com.armanco.formula.view.section.SectionFragment;
+import com.ogaclejapan.smarttablayout.SmartTabLayout;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
+import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
 import java.util.List;
 
 public class MainActivity extends BaseActivity implements MainContract.View {
 
     private MainPresenter presenter = new MainPresenter();
-    private MaterialViewPager materialViewPager;
     private ViewPager viewPager;
 
     private TextView headerTv;
@@ -28,8 +28,6 @@ public class MainActivity extends BaseActivity implements MainContract.View {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        materialViewPager = findViewById(R.id.materialViewPager);
 
         presenter.onAttach(this);
         presenter.onViewCreated();
@@ -44,16 +42,18 @@ public class MainActivity extends BaseActivity implements MainContract.View {
 
     @Override
     public void showPager(final List<Section> sections) {
-        viewPager = materialViewPager.getViewPager();
-        viewPager.setAdapter(new SectionAdapter(getSupportFragmentManager(), sections, this));
-        materialViewPager.getPagerTitleStrip().setViewPager(materialViewPager.getViewPager());
-        headerTv = viewPager.findViewById(R.id.logo_white);
-        materialViewPager.setMaterialViewPagerListener(new MaterialViewPager.Listener() {
-            @Override
-            public HeaderDesign getHeaderDesign(int page) {
-                setStatusBarColor(sections.get(page).colorId);
-                return HeaderDesign.fromColorResAndUrl(sections.get(page).colorId, "");
-            }
-        });
+
+        FragmentPagerItems.Creator creator = FragmentPagerItems.with(this);
+        for(Section section : sections) {
+            creator.add(section.nameId, SectionFragment.class);
+        }
+        FragmentPagerItemAdapter adapter = new FragmentPagerItemAdapter(
+                getSupportFragmentManager(), creator.create());
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(adapter);
+
+        SmartTabLayout viewPagerTab = findViewById(R.id.view_pager_tab);
+        viewPagerTab.setViewPager(viewPager);
     }
 }
--- app/src/main/java/com/armanco/formula/view/section/SectionFragment.java
+++ app/src/main/java/com/armanco/formula/view/section/SectionFragment.java
@@ -8,16 +8,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.armanco.formula.R;
-import com.armanco.formula.data.models.SubSection;
-import com.github.florent37.materialviewpager.header.MaterialViewPagerHeaderDecorator;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class SectionFragment extends Fragment {
 
@@ -29,18 +21,6 @@ public class SectionFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        final List<SubSection> items = new ArrayList<>();
-
-        for (int i = 0; i < 5; ++i) {
-            items.add(new SubSection(R.string.app_name, null));
-        }
-        RecyclerView recyclerView = view.findViewById(R.id.recyclerView);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        recyclerView.setHasFixedSize(true);
-
-        //Use this now
-        recyclerView.addItemDecoration(new MaterialViewPagerHeaderDecorator());
-        recyclerView.setAdapter(new SubSectionAdapter(items, getActivity(), R.layout.fragment_section));
 
     }
 }
